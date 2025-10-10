e3fa53efc5c34365318984b8ddf7f00a143d4c1c
burhanuddindhikaf <dika.firman031@gmail.com>
2025-10-10 15:07:05 +0700

    model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use Illuminate\Auth\Events\Login;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+route::post('/login', [AuthController::class, 'login']);
+route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+route::post('/register', [AuthController::class, 'register']);
+route::post('/logout', [AuthController::class, 'logout']);
+
+// route::middleware(['auth','role:admin'])->prefix('admin')->group(function()
+// {
+//     route::get('/dashboard')
+// }
+//)
